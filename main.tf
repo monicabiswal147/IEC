2b1795093dfb242798a54f25da7dbe27ae80a08f
mbiswal3 <mbiswal3@gatech.edu>
2025-11-09 16:25:15 +1100

    maintain tf state

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-iec-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
